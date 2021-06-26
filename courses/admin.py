d2ed662f39a6b5a154c4c77d9e1900026a4fb37d
Oliver Otchere <oliverotchere4@gmail.com>
2021-06-26 16:29:48 +0000

    courses : add courses model to admin panel

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Course,CourseSection,Rate,Sector,Comment,Episode
 
-# Register your models here.
+
+admin.site.register(Course)
+admin.site.register(CourseSection)
+admin.site.register(Rate)
+admin.site.register(Sector)
+admin.site.register(Comment)
+admin.site.register(Episode)
