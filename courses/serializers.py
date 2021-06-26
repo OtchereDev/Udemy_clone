41dd173cc9217d023a32878a99cce8e8c914cf61
Oliver Otchere <oliverotchere4@gmail.com>
2021-06-26 22:17:45 +0000

    courses : added serializers for courses models

---
+++ courses/serializers.py
@@ -0,0 +1,75 @@
+from users.serializers import UserSerializer
+from courses.models import Comment, Course, CourseSection, Episode
+from rest_framework.serializers import ModelSerializer
+
+class EpisodePaidSerializer(ModelSerializer):
+    class Meta:
+        model=Episode
+        fields=[
+            "title",
+            "file",
+            "length",
+        ]
+
+class CourseSectionPaidSerializer(ModelSerializer):
+    episodes=EpisodePaidSerializer(many=True)
+    class Meta:
+        model=CourseSection
+        fields=[
+            "section_title",
+            "section_number",
+            "episodes",
+        ]
+
+class EpisodeUnPaidSerializer(ModelSerializer):
+    class Meta:
+        model=Episode
+        fields=[
+            "title",
+            "length",
+        ]
+
+class CourseSectionUnPaidSerializer(ModelSerializer):
+    episodes=EpisodeUnPaidSerializer(many=True)
+    class Meta:
+        model=CourseSection
+        fields=[
+            "section_title",
+            "section_number",
+            "episodes",
+        ]
+
+class CommentSerializer(ModelSerializer):
+    user=UserSerializer()
+    class Meta:
+        model=Comment
+        fields=[
+            'user',
+            'message',
+            'created'
+        ]
+
+class CourseUnPaidSerializer(ModelSerializer):
+
+    comment=CommentSerializer(many=True)
+    author=UserSerializer()
+    course_sections=CourseSectionUnPaidSerializer(many=True)
+    class Meta:
+        model=Course
+        exclude=[
+            'rating',
+            
+        ]
+
+class CoursePaidSerializer(ModelSerializer):
+    
+    comment=CommentSerializer(many=True)
+    author=UserSerializer()
+    course_sections=CourseSectionPaidSerializer(many=True)
+    class Meta:
+        model=Course
+        exclude=[
+            'rating',
+           
+        ]
+
