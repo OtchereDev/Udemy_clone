9879b5ddfad96bfc7c85ca701584e7a986c37e03
Oliver Otchere <oliverotchere4@gmail.com>
2021-06-26 14:38:29 +0000

    core : add routing for courses

--- udemy_clone/urls.py
+++ udemy_clone/urls.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('courses',include('courses.urls',namespace='courses'))
 ]
 
 if settings.DEBUG:
