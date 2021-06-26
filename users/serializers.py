c230a575f99dbc4e141bca9cae385ff3f57ceef6
Oliver Otchere <oliverotchere4@gmail.com>
2021-06-26 22:19:57 +0000

    users : added serializer for user model

---
+++ users/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework.serializers import ModelSerializer
+from .models import User
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model=User
+        fields=[
+            "name"
+        ]
