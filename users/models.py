703fa0f08863298867dbfc7e53faaf9c2ca752ce
Oliver Otchere <oliverotchere4@gmail.com>
2021-06-26 16:28:34 +0000

    users : finalized user model

--- users/models.py
+++ users/models.py
@@ -1,38 +1,39 @@
 from courses.models import Course
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser,PermissionManager,BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager, PermissionsMixin
 
 class UserManager(BaseUserManager):
-    def create_superuser(self,email,name,password,**other_fields):
+    use_in_migrations=True
+    def create_superuser(self,email,password,name,**other_fields):
         other_fields.setdefault('is_staff',True)
-        other_fields.setdefault('is_admin',True)
+        other_fields.setdefault('is_superuser',True)
         other_fields.setdefault('is_author',True)
 
 
         if other_fields.get('is_staff') is not True:
             return ValueError('Superuser must be assign is_staff=True')
 
-        if other_fields.get('is_admin') is not True:
-            return ValueError('Superuser must be assign is_admin=True')
+        if other_fields.get('is_superuser') is not True:
+            return ValueError('Superuser must be assign is_superuser=True')
 
         if other_fields.get('is_author') is not True:
             return ValueError('Superuser must be assign is_author=True')
 
-        return self.create_user(email,name,password,**other_fields)
+        return self.create_user(email,password,name,**other_fields)
 
 
-    def create_author(self,email,name,password,**other_fields):
+    def create_author(self,email,password,name,**other_fields):
         other_fields.setdefault('is_staff',False)
-        other_fields.setdefault('is_admin',False)
+        other_fields.setdefault('is_superuser',False)
         other_fields.setdefault('is_author',True)
 
         if other_fields.get('is_author') is not True:
             return ValueError('Author must be assign is_author=True')
 
-        return self.create_user(email,name,password,**other_fields)
+        return self.create_user(email,password,name,**other_fields)
 
 
-    def create_user(self,email,name,password,**other_fields):
+    def create_user(self,email,password,name,**other_fields):
 
         if not email:
             raise ValueError('You must provide a valid email')
@@ -48,8 +49,8 @@ class UserManager(BaseUserManager):
         return user
         
 
-class User(AbstractBaseUser,PermissionManager):
-    name=models.CharField(max_length=225)
+class User(AbstractBaseUser,PermissionsMixin):
+    name=models.CharField(max_length=225,null=True,blank=True)
     email=models.EmailField(max_length=225,unique=True)
     paid_course=models.ManyToManyField(Course,blank=True)
     created=models.DateTimeField(auto_now_add=True)
@@ -57,6 +58,7 @@ class User(AbstractBaseUser,PermissionManager):
     is_active=models.BooleanField(default=True)
     is_staff=models.BooleanField(default=False)
     is_author=models.BooleanField(default=False)
+    # is_superuser=models.BooleanField(default=False)
 
     USERNAME_FIELD='email'
     REQUIRED_FIELDS=['name']
