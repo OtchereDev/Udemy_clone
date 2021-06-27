65f90656b86eda0828eec50badb19372304a66b2
Oliver Otchere <oliverotchere4@gmail.com>
2021-06-27 21:27:14 +0000

    courses: added views for author management of course

--- courses/views.py
+++ courses/views.py
@@ -1,17 +1,21 @@
-from courses.serializers import CoursePaidSerializer, CourseUnPaidSerializer
-from courses.models import Course
+from django.http.response import HttpResponseBadRequest, HttpResponseNotAllowed
+from courses.serializers import CoursePaidSerializer, CourseUnPaidSerializer, SectorSerializer
+from courses.models import Course, Sector
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-class CoursesViews(APIView):
+from rest_framework import status
+
+class CoursesHomeViews(APIView):
     def get(self,request,*args, **kwargs):
-        courses=Course.objects.all()
-        serializer=CourseUnPaidSerializer(courses,many=True)
+        sector=Sector.object.all()
+        serializer=SectorSerializer(sector,many=True)
+        
+        return Response(data=serializer.data,status=status.HTTP_200_OK)
 
-        return Response(data=serializer.data,status=200)
 
-
+class CourseCreateView(APIView):
 
     def post(self,request,*args, **kwargs):
         serializer=CoursePaidSerializer(request.data)
@@ -21,9 +25,100 @@ class CoursesViews(APIView):
             obj=serializer.save(author=request.user)
             print(obj)
 
-            return Response(obj,status=201)
+            return Response(obj,status=status.HTTP_201_CREATED)
         else:
 
-            return Response(data=serializer.errors,status=400)
+            return Response(data=serializer.errors,status=status.HTTP_400_BAD_REQUEST)
             
     
+class CourseSearch(APIView):
+    def get(self,request,sector_uuid,*args, **kwargs):
+        sector=Sector.objects.filter(sector_uuid=sector_uuid)
+
+        if not sector:
+            return HttpResponseBadRequest("Course sector does not exist")
+
+        sector_courses=sector.objects.all()
+
+        serializer=CourseUnPaidSerializer(sector_courses,many=True)
+
+        return Response(serializer.data,status=status.HTTP_200_OK)
+
+
+class CourseDetail(APIView):
+    def get(self,request,course_uuid,*args, **kwargs):
+        
+        course=Course.objects.filter(course_uuid=course_uuid)
+
+        if not course:
+            return HttpResponseBadRequest('Course does not exist')
+
+        serializer=CourseUnPaidSerializer(course[0])
+
+        if serializer.is_valid():
+            return Response(serializer.validated_data,status=status.HTTP_200_OK)
+
+        return HttpResponseBadRequest('Course does not exist')
+
+
+class CourseStudy(APIView):
+    def get(self,request,course_uuid,test=None):
+        check_course=Course.objects.filter(course_uuid=course_uuid)
+
+        if check_course:
+            return HttpResponseBadRequest('Course does not exist')
+
+        user_course=request.user.paid_course.objects.filter(course_uuid=course_uuid)
+
+        if user_course:
+            return HttpResponseNotAllowed("User has not purchased this course")
+
+        course=Course.objects.filter(course_uuid=course_uuid)[0]
+
+        serializer=CoursePaidSerializer(course[0])
+        
+        if serializer.is_valid():
+            return Response(serializer.validated_data,status=status.HTTP_200_OK)
+
+
+        return HttpResponseBadRequest()
+
+
+class CourseManageCourseList(APIView):
+    def get(self,request,*args, **kwargs):
+        courses=Course.objects.filter(author=request.user)
+        serializer=CoursePaidSerializer(courses,many=True)
+
+        return Response(data=serializer.data,status=status.HTTP_200_OK)
+
+
+class CourseManage(APIView):
+
+    def patch(self,request,course_uuid,*args, **kwargs):
+        course=Course.objects.filter(course_uuid=course_uuid,author=request.user)
+
+        if not course:
+            return HttpResponseBadRequest()
+
+        serializer=CoursePaidSerializer(request.data,instance=course[0])
+
+        if serializer.is_valid():
+            data=serializer.save()
+        
+        return Response(data=data,status=status.HTTP_201_CREATED)
+
+
+    def delete(self,request,course_uuid,*args, **kwargs):
+
+        course=Course.objects.filter(course_uuid=course_uuid,author=request.user)
+
+        if not course:
+            return HttpResponseBadRequest()
+
+        course[0].delete()
+
+        return Response(data={
+            "status":"successfull",
+            "course":course[0]
+        },status=status.HTTP_204_NO_CONTENT)
+
