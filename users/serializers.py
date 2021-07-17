dfe7ccf4316bbfc6aaa981e1455fb6f84e2eb52d
Oliver Otchere <oliverotchere4@gmail.com>
2021-07-17 12:33:38 +0000

    add serializer for auth provider

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import User
 
 class UserSerializer(ModelSerializer):
@@ -6,4 +7,16 @@ class UserSerializer(ModelSerializer):
         model=User
         fields=[
             "name"
+        ]
+
+
+class UserAuthSerializer(ModelSerializer):
+    courses=serializers.ListField(source='get_all_courses')
+    class Meta:
+        model=User
+        fields=[
+            'name',
+            'id',
+            'courses',
+            'email'
         ]
