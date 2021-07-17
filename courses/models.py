b8bb43838b8d8e5f95149679ca66a106fd372cc5
Oliver Otchere <oliverotchere4@gmail.com>
2021-07-17 12:41:35 +0000

    added compactibilty for cloudinary

--- courses/models.py
+++ courses/models.py
@@ -2,7 +2,19 @@
 from django.db import models
 from django.core.validators import MaxValueValidator,MinValueValidator
 from django.conf import settings
+from django.contrib.auth import get_user_model
 import uuid
+from mutagen.mp4 import MP4,MP4StreamInfoError
+from .helpers import get_timer
+from decimal import Decimal
+
+from cloudinary_storage.storage import MediaCloudinaryStorage
+from cloudinary_storage.validators import validate_video
+
+from cloudinary.models import CloudinaryField
+
+
+
 
 class Course(models.Model):
     title=models.CharField(max_length=225)
@@ -18,27 +30,48 @@ class Course(models.Model):
     course_sections=models.ManyToManyField('CourseSection',blank=True)
     comment=models.ManyToManyField('Comment',blank=True)
     course_uuid=models.UUIDField(default=uuid.uuid4,unique=True)
+    image_url=models.ImageField(upload_to='course_images',storage=MediaCloudinaryStorage())
+    price=models.DecimalField(max_digits=5 ,decimal_places=2)
 
     def get_rating(self):
-        ratings=self.rating.objects.all()
+        ratings=self.rating.all()
         rate=0
         for rating in ratings:
             rate+=rating.rate_number
-        rate/=len(ratings)
+        try:
+            rate/=len(ratings)
+        except ZeroDivisionError:
+            rate=0
+            
 
         return rate
 
-    def get_short_brief(self):
+    def get_no_rating(self):
+        return len(self.rating.all())
+
+    def get_brief_description(self):
         return self.description[:100]
 
-    # def save(self,*args, **kwargs):
-    #     print(1)
-    #     if len(self.rating.objects.all()):
-    #         print(2)
-    #         rate=self.get_rating()
-    #         print(3)
-    #         self.rate=rate
-    #     return super().save(*args, **kwargs)
+    def get_enrolled_students(self):
+        students=get_user_model().objects.filter(paid_course=self)
+        return len(students)
+
+    def get_total_lectures(self):
+        lectures=0
+        for section in self.course_sections.all():
+            lectures+=len(section.episodes.all())
+        return lectures
+
+    def total_course_length(self):
+        length=Decimal(0.00)
+
+        for section in self.course_sections.all():
+            for episode in section.episodes.all():
+                length+=episode.length
+
+        
+       
+        return get_timer(length,type="short")
 
 
 class Rate(models.Model):
@@ -50,18 +83,42 @@ class CourseSection(models.Model):
     section_number=models.IntegerField(blank=True,null=True)
     episodes=models.ManyToManyField('Episode',blank=True)
 
+    def total_length(self):
+        total=Decimal(0.00)
+        for episode in self.episodes.all():
+            total+=episode.length
+        return get_timer(total,type='min')
+
+
 
 class Episode(models.Model):
     title=models.CharField(max_length=225)
-    file=models.FileField(upload_to='courses')
-    length=models.FloatField(default=0)
+    file=CloudinaryField(resource_type='video',validators=[validate_video],folder='media')
+    # file=models.FileField(upload_to='courses',validators=[validate_video],)
+    length=models.DecimalField(max_digits=100,decimal_places=2)
 
     def get_video_length(self):
-        # get video length here
-        pass
+        try:
+            video=MP4(self.file)
+            return video.info.length
+            
+        except MP4StreamInfoError:
+            return 0.0
+
+    def get_video_length_time(self):
+        return get_timer(self.length)
+    
+    def get_absolute_url(self):
+        return self.file.url
+    
+        
     
     def save(self,*args, **kwargs):
-        # self.length=self.get_video_length()
+        self.length=self.get_video_length()
+        # print(self.length)
+        # print(self.file.path)
+        
+        
         return super().save(*args, **kwargs)
 
 
@@ -75,4 +132,5 @@ class Sector(models.Model):
     name=models.CharField(max_length=225)
     sector_uuid=models.UUIDField(default=uuid.uuid4,unique=True)
     related_courses=models.ManyToManyField(Course,blank=True)
+    sector_image=models.ImageField(upload_to='sector_images',storage=MediaCloudinaryStorage())
 
