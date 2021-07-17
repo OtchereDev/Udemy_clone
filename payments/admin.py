bb598df8554a3a7a036be98d23f591bee440400a
Oliver Otchere <oliverotchere4@gmail.com>
2021-07-17 12:28:14 +0000

    admin for payment

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,5 @@
+from payments.models import Payment, PaymentIntent
 from django.contrib import admin
 
-# Register your models here.
+admin.site.register(Payment)
+admin.site.register(PaymentIntent)
