e5b91c55e9fcb8c7155c9f6895ca74d5073297a9
Oliver Otchere <oliverotchere4@gmail.com>
2021-07-17 12:39:40 +0000

    added view for proj

--- courses/views.py
+++ courses/views.py
@@ -1,22 +1,44 @@
+from django.core.exceptions import ValidationError
+from django.db.models.query_utils import Q
 from django.http.response import HttpResponseBadRequest, HttpResponseNotAllowed
-from courses.serializers import CoursePaidSerializer, CourseUnPaidSerializer, SectorSerializer
+from courses.serializers import CartItemSerializer, CommentSerializer, CourseDisplaySerializer, CourseListSerializer, CoursePaidSerializer, CourseUnPaidSerializer, SectorSerializer
 from courses.models import Course, Sector
-from django.shortcuts import render
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+
+
+from decimal import Decimal
+import json
+
 
 class CoursesHomeViews(APIView):
     def get(self,request,*args, **kwargs):
-        sector=Sector.object.all()
-        serializer=SectorSerializer(sector,many=True)
+        sectors=Sector.objects.order_by('?')[:6]
+
+        sector_response=[]
+
+        for sector in sectors:
+            sector_courses=sector.related_courses.order_by('?')[:4]
+            courses_serializer=CourseDisplaySerializer(sector_courses,many=True)
+            sector_obj={
+                "sector_name": sector.name,
+                "sector_uuid": sector.sector_uuid,
+                "featured_courses": courses_serializer.data,
+                "sector_image":sector.sector_image.url
+            }
+            sector_response.append(sector_obj)
+
+        # serializer=SectorSerializer(sectors=sector_response,many=True)
         
-        return Response(data=serializer.data,status=status.HTTP_200_OK)
+        return Response(data=sector_response,status=status.HTTP_200_OK)
 
 
+# not used yet
 class CourseCreateView(APIView):
-
+    permission_classes=[IsAuthenticated]
     def post(self,request,*args, **kwargs):
         serializer=CoursePaidSerializer(request.data)
 
@@ -38,53 +60,71 @@ class CourseSearch(APIView):
         if not sector:
             return HttpResponseBadRequest("Course sector does not exist")
 
-        sector_courses=sector.objects.all()
+        sector_courses=sector[0].related_courses.all()
 
-        serializer=CourseUnPaidSerializer(sector_courses,many=True)
+        serializer=CourseListSerializer(sector_courses,many=True)
 
-        return Response(serializer.data,status=status.HTTP_200_OK)
+        total_students=0
+
+        for course in sector_courses:
+            total_students+=course.get_enrolled_students()
+
+        
+
+        return Response({'data':serializer.data,
+                        'sector_name':sector[0].name,
+                        'total_students':total_students,
+                        'image':sector[0].sector_image.url},
+                        status=status.HTTP_200_OK)
 
 
 class CourseDetail(APIView):
     def get(self,request,course_uuid,*args, **kwargs):
+        try:
+            
+            course=Course.objects.filter(course_uuid=course_uuid)
+        except ValidationError:
+            return HttpResponseBadRequest('Invalid Course uuid')
         
-        course=Course.objects.filter(course_uuid=course_uuid)
-
         if not course:
             return HttpResponseBadRequest('Course does not exist')
-
+        
         serializer=CourseUnPaidSerializer(course[0])
+        
+        
+        return Response(serializer.data,status=status.HTTP_200_OK)
 
-        if serializer.is_valid():
-            return Response(serializer.validated_data,status=status.HTTP_200_OK)
-
-        return HttpResponseBadRequest('Course does not exist')
+        
 
 
 class CourseStudy(APIView):
-    def get(self,request,course_uuid,test=None):
+    permission_classes=[IsAuthenticated]
+    def get(self,request,course_uuid):
         check_course=Course.objects.filter(course_uuid=course_uuid)
 
-        if check_course:
+        if not check_course:
+            
             return HttpResponseBadRequest('Course does not exist')
 
-        user_course=request.user.paid_course.objects.filter(course_uuid=course_uuid)
+        user_course=request.user.paid_course.filter(course_uuid=course_uuid)
+        
 
-        if user_course:
+        if not user_course:
+            
             return HttpResponseNotAllowed("User has not purchased this course")
 
         course=Course.objects.filter(course_uuid=course_uuid)[0]
 
-        serializer=CoursePaidSerializer(course[0])
+        serializer=CoursePaidSerializer(course)
         
-        if serializer.is_valid():
-            return Response(serializer.validated_data,status=status.HTTP_200_OK)
+        
+        
+        return Response(serializer.data,status=status.HTTP_200_OK)
 
 
-        return HttpResponseBadRequest()
-
-
+# not used yet
 class CourseManageCourseList(APIView):
+    permission_classes=[IsAuthenticated]
     def get(self,request,*args, **kwargs):
         courses=Course.objects.filter(author=request.user)
         serializer=CoursePaidSerializer(courses,many=True)
@@ -92,7 +132,9 @@ class CourseManageCourseList(APIView):
         return Response(data=serializer.data,status=status.HTTP_200_OK)
 
 
+# not used yet
 class CourseManage(APIView):
+    permission_classes=[IsAuthenticated]
 
     def patch(self,request,course_uuid,*args, **kwargs):
         course=Course.objects.filter(course_uuid=course_uuid,author=request.user)
@@ -121,4 +163,83 @@ class CourseManage(APIView):
             "status":"successfull",
             "course":course[0]
         },status=status.HTTP_204_NO_CONTENT)
+
+
+class GetCartDetail(APIView):
+    def post(self,request,*args, **kwargs):
+
+        try:
+            body =  json.loads(request.body)
+        
+        except json.decoder.JSONDecodeError:
+            return HttpResponseBadRequest()
+
+        if type(body.get('cart')) != list:
+            return HttpResponseBadRequest()
+
+
+        if len(body.get("cart")) ==0:
+            return Response(data=[])
+
+        courses=[]
+
+        for uuid in body.get("cart"):
+            item = Course.objects.filter(course_uuid=uuid)
+
+            if not item:
+                return HttpResponseBadRequest()
+            
+            courses.append(item[0])
+
+            # serializer for cart
+        serializer =CartItemSerializer(courses,many=True)
+
+        # TODO : After you have added the price field
+        cart_cost=Decimal(0.00)
+
+        for item in serializer.data:
+           
+            cart_cost+=Decimal(item.get("price"))
+
+
+        return Response(data={"cart_detail":serializer.data,"cart_total":str(cart_cost)})
+
+
+    
+class SearchCourse(APIView):
+
+    def get(self,request,search_term):
+        matches= Course.objects.filter(Q(title__icontains=search_term)|
+            Q(description__icontains=search_term))
+
+        serializer=CourseListSerializer(matches,many=True)
+
+        return Response(data=serializer.data)
+
+
+
+class AddComment(APIView):
+    permission_classes=[IsAuthenticated]
+    def post(self,request,course_uuid,*args, **kwargs):
+        try:
+            course=Course.objects.get(course_uuid=course_uuid)
+        except Course.DoesNotExist:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        content=json.loads(request.body)
+
+        if not content.get('message'):
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+        serializer = CommentSerializer(data=content)
+
+        if serializer.is_valid():
+            comment=serializer.save(user=request.user)
+
+            course.comment.add(comment)
+
+            return Response(status=status.HTTP_200_OK)
+
+        else:
+            return Response(data=serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
