47c2023204b762d1e2173e4380588c0e86bb4500
Oliver Otchere <oliverotchere4@gmail.com>
2021-07-17 12:38:38 +0000

    added all urls for course

--- courses/urls.py
+++ courses/urls.py
@@ -1,11 +1,18 @@
-from courses.views import CourseManage, CoursesHomeViews,CourseManageCourseList
+from courses.views import AddComment, CourseDetail, CourseManage, CourseSearch, CourseStudy, CoursesHomeViews,CourseManageCourseList, GetCartDetail, SearchCourse
 from django.urls import path
 
 
 app_name='courses'
 
 urlpatterns = [
-    path('',CoursesHomeViews.as_view(),),
+    
+    path('cart/',GetCartDetail.as_view()),
+    path('detail/<uuid:course_uuid>/',CourseDetail.as_view()),
+    path("search/<str:search_term>/",SearchCourse.as_view()),
+    path("study/<uuid:course_uuid>/",CourseStudy.as_view()),
+    path('comment/<uuid:course_uuid>/',AddComment.as_view()),
     path('course_management/',CourseManageCourseList.as_view()),
-    path('course_management/<str:course_uuid>/',CourseManage.as_view()),
+    path('course_management/<uuid:course_uuid>/',CourseManage.as_view()),
+    path('',CoursesHomeViews.as_view(),),
+    path('<uuid:sector_uuid>/',CourseSearch.as_view()),
 ]
