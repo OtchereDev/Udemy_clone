cbf4000685894c1369a2d6e1c055add084300c42
Oliver Otchere <oliverotchere4@gmail.com>
2021-07-17 12:30:33 +0000

    models for payment intent and payments

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from users.models import User
+from courses.models import Course
 
-# Create your models here.
+class PaymentIntent(models.Model):
+    payment_intent_id=models.CharField(max_length=225)
+    checkout_id=models.CharField(max_length=225)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    courses=models.ManyToManyField(Course)
+    created=models.DateTimeField(auto_now=True)
+
+class Payment(models.Model):
+    payment_intent=models.ForeignKey(PaymentIntent,on_delete=models.CASCADE)
+    total_amount=models.DecimalField(max_digits=7,decimal_places=2)   
+    created=models.DateTimeField(auto_now=True)
+
+
